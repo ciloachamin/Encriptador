fe6022016fc71d5b56fe529bfeb4259c469c144e
Christopher Loachamin <65583777+ciloachamin@users.noreply.github.com>
2023-01-03 00:23:55 -0500

    Add files

---
+++ README.md
@@ -0,0 +1,43 @@
+# ¡Bienvenidos y Bienvenidas a nuestro primer desafío!
+
+Durante estas cuatro semanas, vamos a trabajar en una aplicación que encripta textos, así podrás intercambiar mensajes secretos con otras personas que sepan el secreto de la encriptación utilizada.
+
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+## **Requisitos:**
+
+- Debe funcionar solo con letras minúsculas
+
+- No deben ser utilizados letras con acentos ni caracteres especiales
+
+- Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
+        Por ejemplo:
+        "gato" => "gaitober"
+        gaitober" => "gato"
+
+- La página debe tener campos parainserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
+
+- El resultado debe ser mostrado en la pantalla.
+
+## Extras:
+
+Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
+
+Tenemos un periodo de tiempo de cuatro semanas para desarrollar el proyecto y vamos a trabajar con el sistema ágil de desarrollo.
+
+Buen proyecto!
+
+
+***VER LANDING PAGES***
+<a href="https://ciloachamin.github.io/Encription/" target="_blank">AQUI</a>
+
+***¡Vámonos hasta el espacio y más allá Explorers!***
+
+<a href="https://github.com/ciloachamin/Alura-Latam" target="_self">Regresar a pagina anterior</a>
---
+++ js/app.js
@@ -0,0 +1,76 @@
+const btnEncrypt = document.getElementById("btn-encriptar");
+const btnDecrypt = document.getElementById("btn-desencriptar");
+const btnCopy=document.getElementById("btn-copiar");
+const inMessage = document.getElementById("textAreas");
+const textEncrypt =document.getElementById("text-result")
+
+btnCopy.style.display="none";
+
+btnEncrypt.addEventListener("click", () => {
+    hasText();
+    textEncrypt.textContent=encrypt(inMessage.value);
+    textEncrypt.style.visibility='visible';
+    inMessage.value="";
+});
+
+function hideElements(){
+  const elements=document.getElementsByClassName("text-decrypt");
+  for (const element of elements) {
+    element.style.display= "none";
+  }
+  btnCopy.style.display="block";
+}
+function showElements(){
+    const elements=document.getElementsByClassName("text-decrypt");
+    for (const element of elements) {
+      element.style.display= "block";
+    }
+    btnCopy.style.display="none";
+}
+
+function hasText() {
+    if (inMessage.value === "") {
+        showElements();
+      }else{
+        hideElements();
+      }
+}
+
+btnDecrypt.addEventListener("click", () => {
+  hasText();
+  textEncrypt.textContent=decrypt(inMessage.value);
+  textEncrypt.style.visibility='visible';
+  inMessage.value="";
+});
+
+btnCopy.addEventListener("click", () => {
+    navigator.clipboard.writeText(textEncrypt.textContent).then(() => {
+        /* clipboard successfully set */
+      }, () => {
+        /* clipboard write failed */
+      });
+});
+
+function encrypt(inMessage) {
+  let encryptedMessage = inMessage;
+
+  encryptedMessage = encryptedMessage.replace(/e/g, "enter");
+  encryptedMessage = encryptedMessage.replace(/i/g, "imes");
+  encryptedMessage = encryptedMessage.replace(/a/g, "ai");
+  encryptedMessage = encryptedMessage.replace(/o/g, "ober");
+  encryptedMessage = encryptedMessage.replace(/u/g, "ufat");
+
+  return encryptedMessage;
+}
+
+function decrypt(inMessage) {
+  let decryptedMessage = inMessage;
+
+  decryptedMessage = decryptedMessage.replace(/enter/g, "e");
+  decryptedMessage = decryptedMessage.replace(/imes/g, "i");
+  decryptedMessage = decryptedMessage.replace(/ai/g, "a");
+  decryptedMessage = decryptedMessage.replace(/ober/g, "o");
+  decryptedMessage = decryptedMessage.replace(/ufat/g, "u");
+
+  return decryptedMessage;
+}
